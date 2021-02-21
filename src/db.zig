aa8a45b4ef0e4beca678f195a176bbbb0ab85327
guillaumederex <gderex8@orange.fr>
2021-02-21 02:27:31 +0100

    Filling db with unicode works

--- src/db.zig
+++ src/db.zig
@@ -1,21 +1,36 @@
 const std = @import("std");
 const sqlite = @import("sqlite");
 
-pub fn createDb() !void {
-    const db = try sqlite.SQLite.open("unicode.db");
-    defer db.close() catch unreachable;
+const db_file_name = "codes.db";
+var database: sqlite.SQLite = undefined;
 
-    var rows = db.exec(
+pub fn checkDbExists() !bool {
+    //Checks if program has access to the db file/if it exists
+    if (std.fs.cwd().access(db_file_name, .{.read = true, .write = true})) {
+        return true;
+    } else |err|
+        return if (err == error.FileNotFound) false else err;
+}
+
+pub fn openDb() !void {
+    database = try sqlite.SQLite.open(db_file_name);
+}
+
+pub fn closeDb() void {
+    database.close() catch unreachable;
+}
+
+pub fn createTable() !void {
+    var ans = database.exec(
         \\ CREATE TABLE chars (
         \\     id INTEGER PRIMARY KEY AUTOINCREMENT,
-        \\     codepoint INTEGER NOT NULL,
         \\     name TEXT NOT NULL,
-        \\     utf8 BLOB NOT NULL
-        \\     user_notes TEXT
+        \\     utf8 BLOB NOT NULL,
+        \\     user_notes TEXT,
         \\     standard_notes TEXT
         \\ );
     );
-    while (rows.next()) |row_item| {
+    while (ans.next()) |row_item| {
         switch (row_item) {
             .Error => |e| {
                 std.debug.warn("sqlite3 errmsg: {s}\n", .{database.errmsg()});
@@ -26,10 +41,11 @@ pub fn createDb() !void {
     }
 }
 
-fn parseFileAndFillDb(file: std.fs.File, database: sqlite.SQLite) !void {
+pub fn parseFileAndFillDb(file: std.fs.File) !void {
     var reader = file.reader();
 
     var buffer: [1024]u8 = undefined;
+    var buf2: [1024]u8 = undefined;
 
     //Read all lines
     while (try reader.readUntilDelimiterOrEof(buffer[0..], '\n')) |line| {
@@ -42,14 +58,13 @@ fn parseFileAndFillDb(file: std.fs.File, database: sqlite.SQLite) !void {
 
         var codepoint = std.mem.tokenize(data, ";").next() orelse return error.IllFormedCodeFile;
         var utf8: [32]u8 = undefined;
-        var codepoint_iterator = std.mem.tokenize(codepoint, ' ');
+        var codepoint_iterator = std.mem.tokenize(codepoint, " ");
         var i: usize = 0;
-        while (codepoint_iterator.next()) |code| {
-            i += std.fmt.unicode.utf8Encode(std.fmt.parseInt(code), utf8[i..]);
-        }
-        
-        var buf2: [1024]u8 = undefined;
-        var request = try std.fmt.bufPrint(buf2[0..1023], "INSERT INTO unicode(codepoint, name) VALUES ('{s}', '{s}');", .{codepoint, name});
+        while (codepoint_iterator.next()) |code|
+            i += try std.unicode.utf8Encode(try std.fmt.parseInt(u21, code, 16), utf8[i..]);
+
+        std.debug.print("{s}\n", .{utf8[0..i]});
+        var request = try std.fmt.bufPrint(buf2[0..1023], "INSERT INTO chars(utf8, name) VALUES (X'{x}', '{s}');", .{utf8[0..i], name});
         buf2[request.len] = 0;
 
         var ans = database.exec(std.mem.spanZ(@ptrCast([*:0]const u8, request.ptr)));
@@ -62,7 +77,34 @@ fn parseFileAndFillDb(file: std.fs.File, database: sqlite.SQLite) !void {
                 else => continue,
             }
         }
-
         //std.debug.print("{x}\n", .{id});
     }
 }
+
+
+
+
+
+
+pub const testing = struct {    //Namespace for testing functions
+    pub fn printSome() !void {
+        var rows = database.exec("SELECT utf8, name FROM chars;");
+
+        while (rows.next()) |row_item| {
+            const row = switch (row_item) {
+                // Ignore when statements are completed
+                .Done => continue,
+                .Row => |r| r,
+                .Error => |e| {
+                    std.debug.warn("sqlite3 errmsg: {s}\n", .{database.errmsg()});
+                    return e;
+                },
+            };
+
+            const codepoint = row.columnText(0);
+            const name = row.columnText(1);
+
+            std.debug.warn("{s}: {s}\n", .{ name, codepoint });
+        }
+    }
+};
--- src/main.zig
+++ src/main.zig
@@ -1,48 +1,25 @@
 const std = @import("std");
-const sqlite = @import("sqlite");   //Available because in build.zig I added this package
+const db = @import("db.zig");
 
 pub fn main() anyerror!void {
-    const db_name = "codes.db";
-
     const cwd = std.fs.cwd(); //Current directory folder
     const stderr = std.io.getStdErr().writer();
 
-    var must_create_db = false;
+    var db_exists = try db.checkDbExists();
 
-    //Checks if program has access to the db file/if it exists
-    cwd.access(db_name, .{.read = true, .write = true}) catch |err| {
-        if (err == error.FileNotFound) {
-            must_create_db = true;
-        } else
-            return err;
-    };
+    try db.openDb();
+    defer db.closeDb();
 
-    const db = try sqlite.SQLite.open(db_name);
-    defer db.close() catch unreachable;
-
-    if (must_create_db) {
+    if (!db_exists) {
         try stderr.print("The codes database does not exist! Creating it from allkeys.txt...\n", .{});
-        try createDb(db);
+        try db.createTable();
+        {
+            const file = try cwd.openFile("allkeys.txt", .{.read = true});
+            try db.parseFileAndFillDb(file);
+        }
         try stderr.print("Done!\n", .{});
     }
 
     //Test
-    var rows = db.exec("SELECT codepoint, comment FROM unicode WHERE id < 500 AND id > 480;");
-
-    while (rows.next()) |row_item| {
-        const row = switch (row_item) {
-            // Ignore when statements are completed
-            .Done => continue,
-            .Row => |r| r,
-            .Error => |e| {
-                std.debug.warn("sqlite3 errmsg: {s}\n", .{db.errmsg()});
-                return e;
-            },
-        };
-
-        const codepoint = row.columnText(0);
-        const comment = row.columnText(1);
-
-        std.debug.warn("{s}: {s}\n", .{ comment, codepoint });
-    }
+    try db.testing.printSome();
 }
